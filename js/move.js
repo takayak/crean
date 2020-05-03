20be0252c52be3bb12319f8c1553a529c840ebd6
takayak <tktknet0530@gmail.com>
2020-05-04 03:31:15 +0900

    add
    add

--- js/move.js
+++ js/move.js
@@ -14,4 +14,17 @@ window.onload = function() {
     }
    });
   }
+
+  // ページ内スクロール
+  $(function(){
+    $('a[href^="#"]').click(function(){
+      var speed = 500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      var position = position - 80; 
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
 };
